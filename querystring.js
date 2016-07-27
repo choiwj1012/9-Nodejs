38274247fa80271410730713eaa11961ef6a182b
choiwj1012 <choiwj1012@hanmail.net>
2016-07-27 09:18:55 +0900

    rename files
    rename files

--- querystring.js
+++
@@ -1,5 +0,0 @@
-var url = require('url');
-var querystring = require('querystring');
-
-var parseObject = url.parse('http://choiwj1012.cafe24.com');
-console.log(querystring.parse(parseObject.query));
