74138a44bb917200a304d1e2e8d53962060aeb3d
WonJae Choi <choiwj1012@hanmail.net>
2016-10-24 11:30:10 +0900

    install express module

---
+++ class/server.js
@@ -0,0 +1,17 @@
+﻿// extract module
+var http = require('http');
+var express = require('express');
+
+// use express
+var app = express();
+app.use(express.static('public'));
+app.use(app.router);
+
+
+
+
+
+// start server
+http.createServer(app).listen(3000, function() {
+	console.log('웹서버 실행 중...http://127.0.0.1:3000');
+});
